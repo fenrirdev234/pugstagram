b9b6e312e28ac5361ab1c3de6621cfc943477cd5
Ciro <ciroalejandro1@gmail.com>
2021-10-23 12:28:03 -0400

    proyecto terminado

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
+
+<script>
+    let actualYear = new Date().getFullYear();
+</script>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">
+            © { actualYear } Pugstagram from Coffeecirodev
+        </div>
+    </div>
+</div>
